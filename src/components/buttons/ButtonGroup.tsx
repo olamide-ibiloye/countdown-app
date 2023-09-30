d72d6213554410e643207c1f57f2eb6dcbd35fba
olamide-ibiloye <ibiloyeolamide@gmail.com>
2023-09-30 17:51:30 +0100

    Updated buttons visibility

--- src/components/buttons/ButtonGroup.tsx
+++ src/components/buttons/ButtonGroup.tsx
@@ -1,14 +1,17 @@
 import { Box } from "@mui/material";
-import React from "react";
+import React, { useContext } from "react";
 import FullScreen from "./FullScreen";
 import PlayAndPause from "./PlayAndPause";
 import Reset from "./Reset";
+import { DataContext } from "../../providers/DataProvider";
 
 const ButtonGroup = () => {
+    const { feature } = useContext(DataContext);
+
     return (
         <Box>
-            <PlayAndPause />
-            <Reset />
+            {feature === "countdown" && <PlayAndPause />}
+            {feature === "countdown" && <Reset />}
             <FullScreen />
         </Box>
     );
